9d758f5887d51522c66da5aa87b7cfea3f4a037d
niv <NivKeidan@gmail.com>
2019-11-17 02:10:05 +0200

    removed commented lines

--- server/httpPayloadTypes/requestPayloadTypes.go
+++ server/httpPayloadTypes/requestPayloadTypes.go
@@ -12,12 +12,10 @@ type JoinGameRequestObject struct {
 }
 
 type AttackRequestObject struct {
-	//AttackingPlayerName string `json:"attackingPlayerName"`
 	AttackingCardCode string `json:"attackingCardCode"`
 }
 
 type DefenseRequestObject struct {
-	//DefendingPlayerName string `json:"defendingPlayerName"`
 	DefendingCardCode string `json:"defendingCardCode"`
 	AttackingCardCode string `json:"attackingCardCode"`
 }
